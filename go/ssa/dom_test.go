8608f61074579f15f3286f051124aa4f9f2d0a58
Milind Chabbi <milind@uber.com>
2021-06-09 20:44:21 -0700

    ssa: include accidentally deleted tests in the previous checkin.

--- go/ssa/dom_test.go
+++ go/ssa/dom_test.go
@@ -52,4 +52,43 @@ func TestBuildPostdomTreeRecover(t *testing.T) {
 			t.Error(fmt.Errorf("In correct parent, expected nil"))
 		}
 	}
+
+	b = []*BasicBlock{&BasicBlock{}, &BasicBlock{}, &BasicBlock{}, &BasicBlock{}, &BasicBlock{}, &BasicBlock{}}
+	b[0].Index = 0
+	b[1].Index = 1
+	b[2].Index = 2
+	b[3].Index = 3
+	b[4].Index = 4
+	b[5].Index = 5
+
+	b[0].Succs = []*BasicBlock{b[3]}
+
+	b[1].Succs = []*BasicBlock{b[4], b[5]}
+	b[1].Preds = []*BasicBlock{b[3]}
+
+	b[2].Preds = []*BasicBlock{b[3]}
+
+	b[3].Succs = []*BasicBlock{b[1], b[2]}
+	b[3].Preds = []*BasicBlock{b[0], b[5]}
+
+	b[4].Preds = []*BasicBlock{b[1]}
+
+	b[5].Succs = []*BasicBlock{b[3]}
+	b[5].Preds = []*BasicBlock{b[1]}
+
+	f.Blocks = b
+	//buildDomTree(&f)
+	buildPostdomTree(&f)
+
+	for _, v := range []*BasicBlock{b[1], b[2], b[3], b[4]} {
+		if v.Ipdom() != nil {
+			t.Error(fmt.Errorf("In correct parent, expected nil"))
+		}
+	}
+
+	for _, v := range []*BasicBlock{b[0], b[5]} {
+		if v.Ipdom() != b[3] {
+			t.Error(fmt.Errorf("In correct parent, expected nil"))
+		}
+	}
 }
